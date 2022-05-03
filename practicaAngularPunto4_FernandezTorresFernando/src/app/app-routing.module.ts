78f3c2c761434e9046605e029d62e607f6cf1a38
Mayko2003 <maykox34f@gmail.com>
2022-05-02 23:54:33 -0300

    a

--- practicaAngularPunto4_FernandezTorresFernando/src/app/app-routing.module.ts
+++ practicaAngularPunto4_FernandezTorresFernando/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Page1Component } from './components/page1/page1.component';
+import { PageaComponent } from './components/pagea/pagea.component';
+import { PagebComponent } from './components/pageb/pageb.component';
+import { PagecComponent } from './components/pagec/pagec.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'pageb', component: PagebComponent },
+  { path: 'pagec', component: PagecComponent},
+  { path: 'page1', component: Page1Component},
+  { path: '**', component: PageaComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- practicaAngularPunto4_FernandezTorresFernando/src/app/app.module.ts
+++ practicaAngularPunto4_FernandezTorresFernando/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { Page1Component } from './components/page1/page1.component';
+import { PageaComponent } from './components/pagea/pagea.component';
+import { PagebComponent } from './components/pageb/pageb.component';
+import { PagecComponent } from './components/pagec/pagec.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    Page1Component,
+    PageaComponent,
+    PagebComponent,
+    PagecComponent
   ],
   imports: [
     BrowserModule,
