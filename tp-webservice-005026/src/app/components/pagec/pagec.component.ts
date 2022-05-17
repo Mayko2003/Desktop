24ee5ceb827f61fa9d7a9b1056931cb2cfdba250
Mayko2003 <maykox34f@gmail.com>
2022-05-17 16:24:52 -0300

    a

--- tp-webservice-005026/src/app/components/pagec/pagec.component.ts
+++ tp-webservice-005026/src/app/components/pagec/pagec.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Translate } from 'src/app/models/translate';
 import { GoogleTranslateService } from 'src/app/services/google-translate.service';
-
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 @Component({
   selector: 'app-pagec',
   templateUrl: './pagec.component.html',
@@ -9,13 +9,17 @@ import { GoogleTranslateService } from 'src/app/services/google-translate.servic
 })
 export class PagecComponent implements OnInit {
 
+  text!:SafeHtml;
+  content:string = "<button class='btn btn-danger'>Click me</button>";
+
   translate!: Translate
-  constructor(private googleTranslateService: GoogleTranslateService) {
+  constructor(private sanitized: DomSanitizer ,private googleTranslateService: GoogleTranslateService) {
     this.translate = new Translate()
     this.translate.sourceLang = ""
     this.translate.targetLang = ""
     this.translate.txtTraducido = ""
     this.translate.txtTraducir = ""
+    this.text = this.sanitized.bypassSecurityTrustHtml(this.content);
   }
 
   ngOnInit(): void {
@@ -25,7 +29,6 @@ export class PagecComponent implements OnInit {
     this.googleTranslateService.translate(this.translate.txtTraducir, this.translate.sourceLang, this.translate.targetLang).subscribe(
       (data) => {
         this.translate.txtTraducido = data.data.translations[0].translatedText
-        alert(this.translate.txtTraducido)
       }
     )
   }
--- tp-webservice-005026/src/app/services/google-translate.service.ts
+++ tp-webservice-005026/src/app/services/google-translate.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
@@ -10,22 +10,18 @@ export class GoogleTranslateService {
   constructor(private _http: HttpClient) {}
 
   translate(text:string,source:string, target:string):Observable<any> {
-    const encodeParams = new URLSearchParams();
-    encodeParams.append('q',text)
-    encodeParams.append('source',source)
-    encodeParams.append('target',target)
     
     const options = {
       method: 'POST',
       url: 'https://google-translate1.p.rapidapi.com/language/translate/v2',
       headers: new HttpHeaders({
         'content-type': 'application/x-www-form-urlencoded',
-        'Accept-Encoding': 'application/gzip',
+        //'Accept-Encoding': 'application/gzip',
         'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com',
         'X-RapidAPI-Key': 'd6ba5d39a4msh29c7b0fb1fd1f99p19e701jsn3e67b484b7d4'
       }),
-      data: encodeParams
     }
-    return this._http.post('https://google-translate1.p.rapidapi.com/language/translate/v2',options)
+    const body = new HttpParams().set('q', text).set('source', source).set('target', target);
+    return this._http.post('https://google-translate1.p.rapidapi.com/language/translate/v2',body,options)
   }
 }
