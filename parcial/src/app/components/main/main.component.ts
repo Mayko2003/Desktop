7265361782baa00ef8395437e0b1b2d1b7c140f9
= <=>
2022-05-27 16:05:03 +0000

    parte 1

---
+++ parcial/src/app/components/main/main.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { Book } from 'src/app/models/book';
+import { ApiService } from 'src/app/services/api.service';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.css']
+})
+export class MainComponent implements OnInit {
+
+  submited:boolean = false;
+  nombre:string = "";
+  books: Book[] = [];
+
+  constructor(private apiService:ApiService) { }
+
+
+  search(text:string){
+    this.apiService.getBooks(text).subscribe(
+      (data:any[])=>{
+        data.forEach((book:any) => {
+          let nuevo = new Book()
+          nuevo.id = book.book_id
+          nuevo.title = book.name
+          nuevo.img = book.cover
+          nuevo.autores = book.authors
+          this.books.push(nuevo)
+        });
+      }
+    )
+    this.submited = true
+  }
+
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ parcial/src/app/services/api.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http:HttpClient) { }
+
+  getBooks(text:string):Observable<any>{
+    const options = {
+      method: "GET",
+      headers: {
+        'X-RapidAPI-Host': 'hapi-books.p.rapidapi.com',
+        'X-RapidAPI-Key': '7f8abb8021msh4f7036ef531c2e6p1b28f9jsn49c6fdee0f8b'
+      }
+    }
+    return this.http.get("https://hapi-books.p.rapidapi.com/search/"+text,options)
+  }
+}
