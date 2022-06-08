9672afdc19a528c519f925dc1e25056af980d9dc
= <=>
2022-06-07 21:40:08 -0300

    configurando frontend

--- tp5-FernandezTorresFernando-backed/frontend/src/app/app-routing.module.ts
+++ tp5-FernandezTorresFernando-backed/frontend/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DivisaComponent } from './components/divisa/divisa.component';
+import { LibroComponent } from './components/libro/libro.component';
+import { PasajeComponent } from './components/pasaje/pasaje.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'divisa', component: DivisaComponent },
+  { path: 'pasaje', component: PasajeComponent },
+  {path: '**', component: LibroComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- tp5-FernandezTorresFernando-backed/frontend/src/app/app.module.ts
+++ tp5-FernandezTorresFernando-backed/frontend/src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LibroComponent } from './components/libro/libro.component';
+import { DivisaComponent } from './components/divisa/divisa.component';
+import { PasajeComponent } from './components/pasaje/pasaje.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LibroComponent,
+    DivisaComponent,
+    PasajeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
